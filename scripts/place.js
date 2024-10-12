eb912f4a124b4f6e09aebc018ca4cfb95778e04a
larapalermo <palermolara9@gmail.com>
2024-10-11 21:43:44 -0300

    initial commit

--- scripts/place.js
+++ scripts/place.js
@@ -8,4 +8,19 @@ const lastModifiedParagraph = document.createElement('p');
 lastModifiedParagraph.textContent = `Last modified: ${document.lastModified}`;
 
 footer.appendChild(copyrightParagraph);
-footer.appendChild(lastModifiedParagraph); 
+footer.appendChild(lastModifiedParagraph);
+
+function WindChill(temp, windspeed) {
+    return 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temp * Math.pow(windSpeed, 0.16));
+}
+const temperature = 30;
+const windSpeed = 10;
+
+const windChillElement = document.querySelector('#wind-chill');
+
+if (temperature <= 50 && windSpeed > 3) {
+    const windChill = WindChill(temperature, windSpeed).toFixed(1);
+    windChillElement.textContent = `${windChill} °F`;
+} else {
+    windChillElement.textContent = 'N/A';
+}
